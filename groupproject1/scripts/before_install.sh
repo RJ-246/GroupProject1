6c6d6d314a6242c906a5ce74eeb016d7c65257d2
ikemelanson <ikemelanson@gmail.com>
2023-11-07 08:49:16 -0700

    add startup scripts

---
+++ groupproject1/scripts/before_install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# clean codedeploy-agent files for a fresh install
+sudo rm -rf /home/ec2-user/install
+
+# install CodeDeploy agent
+sudo yum -y update
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install 
+sudo ./install auto
+
+# update os & install python3
+sudo yum update
+sudo yum install -y python3 python3-dev python3-pip python3-venv
+pip install --user --upgrade virtualenv
+
+# delete app
+sudo rm -rf /home/ec2-user/django-aws_cicd
