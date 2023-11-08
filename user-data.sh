17a6b61636a6183f966b9d9408173b4c365c7176
Ryan Jackson <rmjackson234@gmail.com>
2023-11-07 23:08:32 -0700

    changed appspec directory

---
+++ user-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum update
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
